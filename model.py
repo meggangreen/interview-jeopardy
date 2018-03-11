b92b2c63229d67c86e5e3bb6ec02def96ad9b88a
Meggan M Green <github@meggangreen.com>
2018-03-11 00:59:11 +0000

    @wip db model

--- model.py
+++ model.py
@@ -4,21 +4,21 @@ from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
-class Question(object):
+class Question(db.Model):
     """ Questions model """
 
     __tablename__ = 'questions'
 
     q_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     text = db.Column(db.Text, nullable=False)
-    difficulty = db.Column(db.Enum(1, 2, 3), default=2,
+    difficulty = db.Column(db.Enum('1', '2', '3'), default='2',
                            index=True,
                            nullable=False)
     category_id = db.Column(db.String(1),
                             db.ForeignKey('categories.c_id'),
                             index=True,
                             nullable=False)
-    subject_id = db.Column(db.String(1),
+    subject_id = db.Column(db.Integer,
                            db.ForeignKey('subjects.s_id'),
                            index=True,
                            nullable=False)
@@ -43,3 +43,26 @@ class Question(object):
     # subject
 
 
+def connect_to_db(app):
+    """ Connect the database to the Flask app. """
+
+    # Configure to use PostgreSQL production database
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///cliijeopardy'
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    db.app = app
+    db.init_app(app)
+
+
+if __name__ == '__main__':
+
+    # Start Flask app
+    from flask import Flask
+    app = Flask(__name__)
+
+    # Work directly with DB
+    connect_to_db(app)
+    print "\n\nConnected to DB.\n"
+
+    # Create and seed database, if necessary
+    db.create_all()
+    # seed_data()
