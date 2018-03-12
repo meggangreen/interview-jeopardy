195b95f3bc9ed320039b98ecaa10ee9048b5b568
Meggan M Green <github@meggangreen.com>
2018-03-12 22:17:29 +0000

    models done

--- model.py
+++ model.py
@@ -4,33 +4,29 @@ from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
+################################################################################
 ### Classes ###
 
-class Category(db.Model):
-    """ Categories model
-
-        The rows for Behavioral, Coding, Theoretical are filled in the function
-        seed_categories.
-
-    """
-
-    __tablename__ = 'categories'
-
-    c_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    title = db.Column(db.Text, nullable=False)
-
-
 class Subject(db.Model):
-    """ Subjects model
-
-        The rows are filled in the function 'seed_subjects'.
-
-    """
+    """ Subjects model; filled in the function 'seed_subjects'. """
 
     __tablename__ = 'subjects'
 
     s_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     title = db.Column(db.Text, nullable=False)
+
+
+class Q_Subj(db.Model):
+    """ Association table to manage many-to-many relationship between Subjects
+        and Questions.
+
+    """
+
+    __tablename__ = 'qs_subjs'
+
+    qs_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    s_id = db.Column(db.Integer, db.ForeignKey('subjects.s_id'), nullable=False)
+    q_id = db.Column(db.Integer, db.ForeignKey('questions.q_id'), nullable=False)
 
 
 class Question(db.Model):
@@ -39,28 +35,18 @@ class Question(db.Model):
     __tablename__ = 'questions'
 
     q_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    title = db.Column(db.Text, nullable=False)
     text = db.Column(db.Text, nullable=False)
-    difficulty = db.Column(db.Integer, nullable=False)  # 1, 2, 3
-    category_id = db.Column(db.Integer,
-                            db.ForeignKey('categories.c_id'),
-                            index=True,
-                            nullable=False)
-    # subject_id = db.Column(db.Integer,
-    #                        db.ForeignKey('subjects.s_id'),
-    #                        index=True,
-    #                        nullable=False)
-    category = db.relationship('Category',
-                               backref=db.backref('questions'))
-    # subjects = db.relationship('Subject',
-    #                            backref=db.backref('questions'))
+    difficulty = db.Column(db.Integer, nullable=False, default=2)  # 1, 2, 3
+    durations = db.Column(db.Text, nullable=False, default="120,")
+    category = db.Column(db.String(1), nullable=False)  # B, T, C
+    subjects = db.relationship('Subject',
+                               order_by='Subject.s_id',
+                               secondary='qs_subjs',
+                               backref=db.backref('questions', order_by=q_id))
 
     def __repr__(self):
-        return ("<Question id={} category={} subject={} difficulty={}\n" +
-                " " * 10 + "text: '{}'>").format(self.q_id,
-                                                 self.category.title,
-                                                 self.subjects,
-                                                 self.difficulty,
-                                                 self.text[:50])
+        return ('<Question id={} "{}">').format(self.q_id, self.title[:60])
 
     @classmethod
     def do_something(cls):
@@ -70,14 +56,48 @@ class Question(db.Model):
     # subject
 
 
+class User(db.Model):
+    """ Users model. Users have IDs and scores. We don't need much on them. """
+
+    __tablename__ = 'users'
+
+    u_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    scores = db.relationship('Scores')
+
+
+class Scores(db.Model):
+    """ Scores model. Holds the various point levels (0 to 5) a user has scored
+        on a specific question. The scores are presented as a comma-separated
+        list and kept in chronologogical order of least-to-most recent.
+        Eg: u_id 4291 + q_id 375 >> '2,2,3,4,4,3,5'
+        Eg: u_id 4291 + q_id 573 >> '4,3,4,5,5,4'
+        Eg: u_id 1942 + q_id 375 >> '3,4,5,4,5'
+
+    """
+
+    __tablename__ = 'scores'
+
+    score_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    q_id = db.Column(db.Integer, db.ForeignKey('questions.q_id'), nullable=False)
+    u_id = db.Column(db.Integer, db.ForeignKey('users.u_id'), nullable=False)
+    points = db.Column(db.Integer, nullable=False)  # 0 to 5
+    count = db.Column(db.Integer, nullable=False, default=0)
+
+
+################################################################################
+### Functions ###
+
 def connect_to_db(app):
     """ Connect the database to the Flask app. """
 
-    # Configure to use PostgreSQL production database
+    # Configure to use PostgreSQL database
     app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///cliijeopardy'
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     db.app = app
     db.init_app(app)
+
+
+
 
 
 if __name__ == '__main__':
