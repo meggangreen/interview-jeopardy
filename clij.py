3a2d8ed08ebfcad062dca1ace5895c6e8c960f61
Meggan M Green <github@meggangreen.com>
2018-03-15 22:08:01 +0000

    wrote outline of flow

--- clij.py
+++ clij.py
@@ -7,4 +7,20 @@ from model import *
 app = Flask(__name__)
 connect_to_db(app)
 
-
+# Functions:
+# start
+#   enter username
+#   check if db exists
+#       no:
+#           create
+#            OR
+#           quit
+#   welcome user
+#       user.create if necessary
+#   rules
+#       q at any time
+#   while play == yes
+#       play game
+#           GAME STEPS
+#           play again?
+#   exit (q at any time)
